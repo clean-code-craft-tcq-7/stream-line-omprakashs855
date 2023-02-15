2a45c73cee644ece3e12f2e588fbab5d76773c6b
Singh Om Prakash (MS/ESX8-INT-XC) <iom2kor@bosch.com>
2023-02-15 19:45:31 +0530

    (TDD-2) Passing the test case and printing value in console

--- src/bms_sender.py
+++ src/bms_sender.py
@@ -2,6 +2,7 @@ import subprocess
 import random
 import os
 import json
+import re
 
 class BMS_Sender:
 
@@ -10,7 +11,7 @@ class BMS_Sender:
                                     stdout=subprocess.PIPE).communicate()[0].rstrip().decode('utf-8')
         self.sender_tc_json_path = os.path.join(self.root_dir, "inc", "sender_test_case.json")
         self.sender_temp_limit_json_path = os.path.join(self.root_dir, "inc", "sender_temperature_limit.json")
-        self.sender_console_out_json_path = os.path.join(self.root_dir, "inc", "sender_console_output.json") 
+        self.test_regex = r"Sender\s\d+\s\-\sB\d+\s[\w\-\s\,]+\|"
         
     def get_json_data(self, path):
         data = {}
@@ -18,16 +19,40 @@ class BMS_Sender:
             with open(path, 'r') as jsonread:
                 data = json.load(jsonread)
         return data
-
-    def celcius_to_Fahr(self, C_temp):
-        # Convert Celcius to Fahrenheit
-        F_temp = (C_temp*(9/5)) + 32
-        return F_temp
-    
-    def Fahr_to_celcius(self, F_temp):
-        # Convert Fahrenheit to Celcius
-        C_temp = (F_temp - 32)*(5/9)
-        return C_temp
     
     def create_random_value(self, min, max):
         return random.randint(min, max)
+    
+    def select_random_battery(self, no_of_sender_battery):
+        return self.create_random_value(0, no_of_sender_battery-1)
+        
+    def create_temp_stream_output(self, idx, battery_idx, bat):
+        temp_limit_json_data = self.get_json_data(self.sender_temp_limit_json_path)
+        min_temp = temp_limit_json_data["Temperature_Limit"]["Min"]
+        max_temp = temp_limit_json_data["Temperature_Limit"]["Max"]
+        rand_temp_out = self.create_random_value(min_temp, max_temp)
+        out_data = "Sender {} - B{} - {}, {}, {} |".format(battery_idx+1, idx+1, bat["Battery_Type"], bat["Temp_Type"], rand_temp_out)
+        print(out_data)
+        return out_data
+    
+    def create_stream_output(self, test_case_json_data):
+        console_out_data = []
+        # test_case_json_data = self.get_json_data(self.sender_tc_json_path)
+        for idx in range(0, test_case_json_data["Stream_Length"]):
+            battery_idx = self.select_random_battery(test_case_json_data["Number_Of_Batteries"])
+            battery_config = test_case_json_data["Batteries_List"][battery_idx]
+            console_out_data.append(self.create_temp_stream_output(idx, battery_idx, battery_config))
+        
+        return console_out_data
+    
+    def regex_match(self, console_out):
+        match_check = True
+        for str in console_out:
+            if not re.match(self.test_regex, str):
+                match_check = False
+        return match_check
+
+
+
+
+
--- test/bms_sender.test.py
+++ test/bms_sender.test.py
@@ -1,25 +1,21 @@
 import unittest
 import os
 import sys
-import re
 
 sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
 from src.bms_sender import BMS_Sender as BS
 
 class Test_BMS_Sender(unittest.TestCase):
-    
-    def __init__(self):
-        self.test_regex = r"Sender\s\w+\-\sB\d+\s[\w\-\s\,]+\|"
 
     def test_bms_sender_console_regex(self):
 
         # Creating BMS_Sender Class Object
         sender_obj = BS()
-        
-        sender_console_data = sender_obj.get_json_data(sender_obj.sender_console_out_json_path)
-        
-        for data in sender_console_data:
-            self.assertRegex(data, self.test_regex)
 
+        json_data = sender_obj.get_json_data(sender_obj.sender_tc_json_path)
+        for tc in json_data["Sender"]:
+            console_out = sender_obj.create_stream_output(tc)
+            self.assertTrue(sender_obj.regex_match(console_out))
+        
 if __name__ == "__main__":
     unittest.main()
